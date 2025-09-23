db9ef6166dae7087d9a880ff3b0a040b5cf1c5bb
Enrique <you@example.com>
2025-09-23 10:38:21 -0400

    configuracion vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { AppProvider } from './contexts/AppContext';
 import './index.css';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppProvider>
       <App />
+      <SpeedInsights />
     </AppProvider>
   </StrictMode>
 );
